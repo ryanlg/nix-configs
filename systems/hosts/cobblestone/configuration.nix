4f90e5ef470feea3811925857ca1d54d15722794
Ryan Liang <ryan@ryanl.io>
2025-07-12 23:02:15 +1000

    v2

--- systems/hosts/cobblestone/configuration.nix
+++ systems/hosts/cobblestone/configuration.nix
@@ -8,8 +8,8 @@
 {
   imports = [
     ./hardware-configuration.nix
-    ./disk-config.nix
-    ../../modules
+    # ./disk-config.nix
+    ../../_modules
   ];
 
   # Use the systemd-boot EFI boot loader.
@@ -17,49 +17,20 @@
   boot.loader.efi.canTouchEfiVariables = true;
 
   # networking.hostName = "nixos"; # Define your hostname.
-  # Pick only one of the below networking options.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   networking.networkmanager.enable = true; # Easiest to use and most distros use this by default.
 
   # Set your time zone.
   time.timeZone = "Australia/Melbourne";
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable the X11 windowing system.
-  # services.xserver.enable = true;
-
-  # Configure keymap in X11
-  # services.xserver.xkb.layout = "us";
-  # services.xserver.xkb.options = "eurosign:e,caps:escape";
-
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
-
-  # Enable sound.
-  # hardware.pulseaudio.enable = true;
-  # OR
-  # services.pipewire = {
-  #   enable = true;
-  #   pulse.enable = true;
-  # };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.ryan = {
     isNormalUser = true;
-    initialHashedPassword = "1234";
-    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+    initialPassword = "a";
+    extraGroups = [ "wheel" ];
     packages = with pkgs; [ ];
   };
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
   environment.systemPackages = with pkgs; [
+    git
     curl
     vim
     htop
@@ -67,27 +38,16 @@
     wget
   ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+  # Mount the Nix configs shared by the host.
+  # This assumes macOS host and Apple Virtualization.
+  fileSystems = {
+    "/mnt/nixos" = {
+      device = "share";
+      fsType = "virtiofs";
+    };
+  };
 
-  # List services that you want to enable:
 
-  # Enable the OpenSSH daemon.
   services.openssh.enable = true;
-
-  # Open ports in the firewall.
   networking.firewall.allowedTCPPorts = [ 22 ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
 }
