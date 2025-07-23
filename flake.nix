a44fbc03aa8015478c0f147e52d986e562fac9f6
Ryan Liang <ryan@ryanl.io>
2025-07-23 17:22:18 +1000

    upgrade to 25.05

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
 
   inputs = {
     # nixpkgs: stable and unstable
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
     # home-manager: manage user homes
