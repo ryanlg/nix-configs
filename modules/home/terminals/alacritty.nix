4823344830e09aaafad568db35c4cc7756e272c4
Ryan Liang <ryan@ryanl.io>
2025-12-26 09:57:55 +1100

    alacritty done, colors might change later

--- modules/home/terminals/alacritty.nix
+++ modules/home/terminals/alacritty.nix
@@ -12,7 +12,31 @@ in
     programs.alacritty = {
       enable = true;
       package = cfg.package;
-      theme = "rose_pine";
+      theme = "sonokai";
+      settings = {
+        window = {
+          # Title bar, transparent background and no title bar buttons.
+          decorations = "None";
+          # This makes key combos like alt+b for ncurses movement work
+          option_as_alt = "OnlyLeft";
+          # The window will be maximized on startup.
+          startup_mode = "Maximized";
+          padding = {
+            x = 3;
+            y = 2;
+          };
+        };
+        font = {
+          # @todo: make dynamic
+          normal.family = "MesloLGM Nerd Font";
+          size = 12;
+        };
+        colors = {
+          primary.background = "#191919";
+          normal.cyan = "#85b2d6";
+        };
+        mouse.hide_when_typing = true;
+      };
     };
   };
 }
