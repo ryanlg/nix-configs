ad4b2c4ef3f849312607878feeba0deddad3ecc9
Ryan Liang <ryan@ryanl.io>
2026-01-01 10:05:33 +0800

    home(zellij): add zjstatus; add theme for it

--- modules/home/multiplexers/zellij/default.nix
+++ modules/home/multiplexers/zellij/default.nix
@@ -7,6 +7,8 @@
   };
 
   xdg.configFile."zellij/config.kdl".source = ./config/config.kdl;
-  xdg.configFile."zellij/layouts".source = ./config/layouts;
+  xdg.configFile."zellij/layouts/default.kdl".source = pkgs.replaceVars ./config/layouts/default.kdl {
+    ZELLIJ_CONFIG_DIR = "${config.xdg.configHome}/zellij";
+  };
   xdg.configFile."zellij/plugins/zjstatus.wasm".source = "${pkgs.zjstatus}/bin/zjstatus.wasm";
 }
