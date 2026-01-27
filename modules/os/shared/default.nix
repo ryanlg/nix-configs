d47f1af3a1e06fe5d7b4787026ee1ba27a7ca85e
Ryan Liang <ryan@ryanl.io>
2026-01-27 20:57:59 +1100

    os(shared): init shared programs
    These programs should be the most basic system-wide tooling. For now we
    only have vim, curl and git.

--- modules/os/shared/default.nix
+++ modules/os/shared/default.nix
@@ -2,6 +2,7 @@
 {
   imports = [
     ./nix.nix
+    ./programs.nix
     ./timezone.nix
   ];
 }
---
+++ modules/os/shared/programs.nix
@@ -0,0 +1,24 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  cfg = config.mySystem.shared.programs;
+in
+{
+  options.mySystem.shared.programs.enable = lib.mkOption {
+    type = lib.types.bool;
+    default = true;
+    description = "Enable shared system programs";
+  };
+
+  config = lib.mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [
+      git
+      vim
+      curl
+    ];
+  };
+}
