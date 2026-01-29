6baab58a1d732c4efc954e2facdaf6a22722f56b
Ryan Liang <ryan@ryanl.io>
2026-01-29 23:14:53 +1100

    home(editors/nvim): fix defaultEditor
    defaultEditor option doesn't set $VISUAL so it doesn't work for programs
    such as git. This can be removed later on when we upgrade nixvim.

--- modules/home/editors/neovim/default.nix
+++ modules/home/editors/neovim/default.nix
@@ -14,6 +14,7 @@ in
     programs.nixvim = {
       enable = true;
       vimAlias = true;
+      # This doesn't seem to work... $VISUAL is stuck at the default.
       defaultEditor = true;
 
       imports = [
@@ -27,6 +28,12 @@ in
       # nixpkgs-25.11 has this plugin at 2025-11-19 and git status auto-refresh
       # doesn't work.
       plugins.neo-tree.package = pkgs-unstable.vimPlugins.neo-tree-nvim;
+    };
+
+    # @upgrade: this is added to nixvim on 2025/12/12
+    # https://github.com/nix-community/nixvim/commit/1e09168
+    home.sessionVariables = {
+      VISUAL = "nvim";
     };
   };
 }
